3b02b6acc49c761068cb00e91f71de435860527a
Shinigamy19 <67855699+Shinigamy19@users.noreply.github.com>
2023-07-22 16:00:12 -0300

    Audio Funcional. Pequeño bug al mutear

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public Toggle muteToggle;
+
+    void Start()
+    {
+        // Inicializa el valor del slider y del toggle con los valores guardados en PlayerPrefs
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1);
+        muteToggle.isOn = PlayerPrefs.GetInt("Mute", 0) == 1;
+    }
+
+    public void OnVolumeChanged()
+    {
+        // Actualiza el volumen del AudioListener cuando se cambia el valor del slider
+        AudioListener.volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+    }
+
+    public void OnMuteChanged()
+    {
+        // Actualiza el muteo del AudioListener cuando se cambia el valor del toggle
+        AudioListener.pause = muteToggle.isOn;
+        PlayerPrefs.SetInt("Mute", muteToggle.isOn ? 1 : 0);
+    }
+}
