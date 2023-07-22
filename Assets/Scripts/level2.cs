488bb57f195d93953d67ab073137f6fcf073c93f
Shinigamy19 <67855699+Shinigamy19@users.noreply.github.com>
2023-07-22 19:02:00 -0300

    Primera Demo

---
+++ Assets/Scripts/level2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2 : Collidable
+{
+    public void Level_2() 
+    {
+        
+    }
+
+     protected override void OnCollide(Collider2D col)
+     {
+        // Verificar si el objeto con el que colisionó tiene el nombre "Player" y si la puerta está bloqueada
+        if (col.name == "Player")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+     }
+}
