6f1d8c411ec410ad19dacc9d6881f5a1b8dd69b6
Shinigamy19 <67855699+Shinigamy19@users.noreply.github.com>
2023-07-23 11:03:19 -0300

    Sonidos Puertas

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    public AudioClip openSound; // Sonido de apertura de la puerta.
+    public AudioClip closeSound; // Sonido de cierre de la puerta.
+    private AudioSource audioSource; // Referencia al componente AudioSource de la puerta.
+
     public void Open(){
+        Invoke("PlaySound", 3);
         gameObject.SetActive(false);
+        
+    }
+
+    public void PlaySound()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(openSound);
     }
 
     public void Close(){
         gameObject.SetActive(true);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(closeSound);
     }
 }
