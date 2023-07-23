e38fa3fc6611711a4f51c4b2ab79a8e39a491afa
Shinigamy19 <67855699+Shinigamy19@users.noreply.github.com>
2023-07-23 02:44:39 -0300

    Respawn correcion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class Player : Mover
 {
     private void FixedUpdate(){  
@@ -13,9 +12,13 @@ public class Player : Mover
 
         UpdateMotor(new Vector3(x, y, 0));          
     }
+//Con esto pasamos el nombre de la escena a texto
+    private string currentScene;
 
     protected override void Death(){
-        SceneManager.LoadScene("Level1");
+        currentScene = SceneManager.GetActiveScene().name; // Almacenamos el nombre de la escena actual
+        SceneManager.LoadScene(currentScene);//Cargamos la escena actual cuando muere
+        //SceneManager.LoadScene("Level1");
     }
 
 }
